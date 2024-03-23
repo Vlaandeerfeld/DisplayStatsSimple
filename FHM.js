ca905114a045745ba79e2e87be0ba2bbb7a462ba
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-03-22 22:03:27 -0230

    UPDATE team stats

--- FHM.js
+++ FHM.js
@@ -1,157 +1,82 @@
-async function upload(){
-
-	const input = await document.getElementById('CSVfile');
-	const input1 = await input.files;
-	for (let x = 0; x < input1.length; x++){
-		const reader = await new FileReader();	
-		await reader.readAsText(input1[x]);
-	
-		reader.onload = function(){
-			let input2 = reader.result;
-			fileName = input1[x].name.slice(0, input1[x].name.indexOf('.') - 4);
-			fileDate = input1[x].name.slice(input1[x].name.indexOf('.') - 4, input1[x].name.indexOf('.'));
-			checkAndUpload(input2, fileName, fileDate);
-		}
-	}
-    location.reload();
-}
-
-async function uploadTemplate(){
-
-	let templateLeagues = ['Retro Goon League1980.csv', 'Netherton Hockey League2033.csv'];
-
-	templateLeagues.forEach(files => {
-		let data = fetch('csvtoupload/' + files);
-		let upload = data.text()
-		fileName = files.slice(0, files.indexOf('.') - 4);
-		fileDate = files.slice(files.indexOf('.') - 4, files.indexOf('.'));
-		checkAndUpload(upload, fileName, fileDate);
-	})
-
-	location.reload();
-}
-function checkAndUpload(fileInput, league, date){
-
-    let arrayBreak = [];
-    let continueThrough;
-	console.log(fileInput);
-	console.log(date);
-    JSONparsed = JSON.parse(parseIntoJSON(fileInput, league, date));
-
-	if (localStorage['leagues'] == undefined){
-		localStorage.setItem('leagues', 'ALL');
-	}
-
-	leagueCheck = localStorage['leagues'].split(',');
-
-	leagueCheck.forEach(value => {
-		if (value == league) {
-			if (JSON.stringify(localStorage[league]) == JSON.stringify(JSONparsed.toString())){
-				arrayBreak.push(true);
-			}
-			else{
-				arrayBreak.push(false);
-			}
-		}
-		else{
-			arrayBreak.push(false);
-		}
-	});
-    for (let x = 0; x < arrayBreak.length; x++){
-		if (arrayBreak[x] == true){
-			continueThrough = false;
-			break;
-		}
-		else{
-			continueThrough = true;
-		}
-	}
-    if (continueThrough == true){	
-		localStorage.setItem(league, JSON.stringify(JSONparsed));
-		leagueCheck.push(league);
-		localStorage.setItem('leagues', leagueCheck);
-        console.log(localStorage[league]);
-	}
-
-	FileReader.abort
-}
-
-function parseIntoJSON(fileInput, league, date){
-
-    let finalArray = [];
-    let row = fileInput.slice(fileInput.indexOf("\n") + 1).split("\n");
-    row = row.slice(0, -1);
-    let headers1 = fileInput.slice(0, fileInput.indexOf("\n")).split(",");
-    let place = -1;
-    let key;
- 
-    row.forEach(value2 => {
-        let obj = {};
-        let count = 0;
-        place++;
-        let elementValue = value2.split(',');
-        headers1.forEach(value => {
-            key = value;
-            obj[key] = elementValue[count];
-            count++;
-        });
-		obj['League'] = league;
-		obj['Season'] = date;
-        finalArray.push(obj);
-    });
-
-    return(JSON.stringify(finalArray));
-}
-
-function leagueFilters(page){
+async function leagueFilters(){
 	let outputHTML = '';
-	leaguesToFilter = localStorage['leagues'].split(',');	
-	leaguesToFilter.slice(1).forEach(value => {
-		outputHTML += `<option value = ` + value + `>` + value + `</option>`;
-	})
+
+	outputHTML += `<option value ='ALL'>ALL</option>`;
+
+	await fetch('http://192.168.2.12:3000/fhmleague')
+		.then(response => response.json())
+		.then(data => {
+			console.log(data)
+			data.map(value => {
+				return outputHTML += `<option value = ` + value.LeagueId + `>` + value.AbbrLeague + `</option>`;
+			})
+		});
 
 	document.getElementById('FHMleaguesstats').innerHTML = outputHTML;
 }
 
-function teamFilters(league){
+async function teamFilters(league){
 	let outputHTML = '';
 
-	outputHTML += `<option value = ` + 'ALL' + `>` + 'ALL' + `</option>`;
+	outputHTML += `<option value ='ALL'>ALL</option>`;
 
-	JSON.parse(localStorage[league]).forEach(value => {
-		outputHTML += `<option value = ` + value.Abbr + `>` + value.Abbr + `</option>`;
+	await fetch('http://192.168.2.12:3000/fhmteams', {
+		headers: {
+			league: league
+		}
 	})
+		.then(response => response.json())
+		.then(data => {
+			console.log(data)
+			data.map(value => {
+				return outputHTML += `<option value = ` + value.TeamId + `>` + value.Abbr + `</option>`;
+			})
+		});
 	
 	document.getElementById('FHMteamsstats').innerHTML = outputHTML;
 }
 
-function seasonFilters(league){
+async function seasonFilters(league){
 	let outputHTML = '';
-	team = JSON.parse(localStorage[league]);
-	
-	outputHTML += `<option value = ` + team[0].Season + `>` + team[0].Season + `</option>`;
+
+	outputHTML += `<option value ='ALL'>ALL</option>`;
+
+	await fetch('http://192.168.2.12:3000/fhmteams', {
+		headers: {
+			league: league
+		}
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+		data.map(value => {
+			return outputHTML += `<option value = ` + value.Season + `>` + value.Season + `</option>`;
+		})
+	});
 
 	document.getElementById('FHMseasonstats').innerHTML = outputHTML;
 }
 
-function teamTablesOverview(league, season, team){
-	let data = JSON.parse(localStorage[league]);
-	league = league.toString();
-    console.log(league);
-	let outputHTML = '';
-	outputHTML += `<tbody><tr><th onclick = 'sortTable("teamTablesOverview", 0)'>Team</th><th onclick = 'sortTableNum("teamTablesOverview", 1)'>GP</th><th onclick = 'sortTableNum("teamTablesOverview", 2)'>Wins</th><th onclick = 'sortTableNum("teamTablesOverview", 3)'>Losses</th><th onclick = 'sortTableNum("teamTablesOverview", 4)'>Ties</th><th onclick = 'sortTableNum("teamTablesOverview", 5)'>OTL</th><th onclick = 'sortTableNum("teamTablesOverview", 6)'>Points</th><th onclick = 'sortTableNum("teamTablesOverview", 7)'>PCT</th><th onclick = 'sortTableNum("teamTablesOverview", 8)'>G</th></tr>`;
-	data.forEach(value => {
-		if (team != 'ALL'){
-			if (value.Season == season && value.Abbr == team){
-				outputHTML += `<tr><td onclick = 'localStorage["currentLeague"] = document.getElementById("FHMleaguesstats").options[document.getElementById("FHMleaguesstats").options.selectedIndex].text; localStorage["currentTeam"] = document.getElementById("FHMteamsstats").options[document.getElementById("FHMteamsstats").options.selectedIndex].text; window.location.href = "FHMstatsteam.html"'>${value.Abbr}</td><td>${value.GP_RS}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.Ties}</td><td>${value.OTL}</td><td>${value.Points}</td><td>${value.PCT}</td><td>${value.G_RS}</td></tr>`;
-			}
+async function teamTablesOverview(league, season, team){
+	outputHTML = '';
+	outputHTML += `<tbody><tr><th onclick = 'sortTable("teamTablesOverview", 0)'>City</th><th onclick = 'sortTable("teamTablesOverview", 1)'>Team</th><th onclick = 'sortTableNum("teamTablesOverview", 2)'>GP</th><th onclick = 'sortTableNum("teamTablesOverview", 3)'>Wins</th><th onclick = 'sortTableNum("teamTablesOverview", 4)'>Losses</th><th onclick = 'sortTableNum("teamTablesOverview", 5)'>OTL</th><th onclick = 'sortTableNum("teamTablesOverview", 6)'>Points</th></tr>`;
+	
+	console.log(team);
+	await fetch('http://192.168.2.12:3000/fhmteam', {
+		headers:{
+			season: season,
+			team: team,
+			league: league,
+			gametype: 'Regular Season'
 		}
-		else{
-			if (value.Season == season){
-				outputHTML += `<tr><td id = ${value.Abbr} onclick = 'localStorage["currentLeague"] = document.getElementById("FHMleaguesstats").options[document.getElementById("FHMleaguesstats").options.selectedIndex].text; localStorage.setItem("currentTeam", document.getElementById("${value.Abbr}").innerHTML); window.location.href = "FHMstatsteam.html";'>${value.Abbr}</td><td>${value.GP_RS}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.Ties}</td><td>${value.OTL}</td><td>${value.Points}</td><td>${value.PCT}</td><td>${value.G_RS}</td></tr>`;
-			}
-		}
-	});
+	})
+	.then(response => response.json())
+	.then(data => {
+		data.map(value => {
+			outputHTML += `<tr><td>${value.city}<td id = ${value.teamName} onclick = 'localStorage["currentLeague"] = document.getElementById("FHMleaguesstats").options[document.getElementById("FHMleaguesstats").options.selectedIndex].text; localStorage.setItem("currentTeam", document.getElementById("${value.teamName}").innerHTML); window.location.href = "FHMstatsteam.html";'>${value.teamName}</td><td>${value.GP}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.OTL}</td><td>${value.Points}</td></tr>`;
+		})
+	})
+
     outputHTML += `</tbody>`;
 
     document.getElementById('teamTablesOverview').innerHTML = outputHTML;
